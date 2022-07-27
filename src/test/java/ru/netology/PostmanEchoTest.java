207083229ab7ae99c66dc7cd81f9a446ed35be18
mashakulina <masha_kulina@mail.ru>
2022-07-27 19:33:37 +0300

    add new ver PostmanEchoTest

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("some data"))
         ;
     }
 }
